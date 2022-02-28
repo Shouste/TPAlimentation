772b78270facbfd19f8553c56368ce48bc17927a
Sandrine Houste <shouste@gmail.com>
2022-02-28 09:33:33 +0100

    Initial commit

---
+++ src/main/java/com/bnpparibas/lafabrique/TPAlimentation/TpAlimentationApplication.java
@@ -0,0 +1,13 @@
+package com.bnpparibas.lafabrique.TPAlimentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpAlimentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpAlimentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnpparibas/lafabrique/TPAlimentation/TpAlimentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnpparibas.lafabrique.TPAlimentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpAlimentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
